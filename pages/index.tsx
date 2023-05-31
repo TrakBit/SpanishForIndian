1cc394eb366de53fff56a2a52aef593e49c52699
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-05-31 23:15:11 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>{`Spanish learning for Indians`}</title>
         </Head>
         <Container>
           <Intro />
